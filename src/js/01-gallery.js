df95456dae7fc80caa78a3c276f195c6be3ea941
basesnel <basesnel@gmail.com>
2022-12-12 15:58:35 -0500

    Homework task 02 in process.

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,29 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+
+// Render gallery items.
+gallery.insertAdjacentHTML(
+  'beforeend',
+  galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `
+        <a class="gallery__item" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        `
+    )
+    .join('')
+);
+
+// Simple-lightbox init.
+const options = { captionsData: 'alt', captionDelay: 250 };
+
+var lightbox = new SimpleLightbox('.gallery a', options);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
 
+const VIDEO_CURRENT_TIME = 'videoplayer-current-time';
+// console.log(localStorage);
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+console.log(player);
+
+const currentTime = localStorage.getItem(VIDEO_CURRENT_TIME);
+console.log(currentTime);
+
+if (currentTime) player.setCurrentTime(currentTime);
+
+const onplay = function (data) {
+  // console.log(data.duration);
+  // console.log(data.percent);
+  // console.log(data.seconds);
+  localStorage.setItem(VIDEO_CURRENT_TIME, data.seconds);
+};
+
+player.on('timeupdate', onplay);
